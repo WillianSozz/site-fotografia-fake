287fcaf47362b2d53d67ddd0692e34ad99cbd508
Willian Sozz <131213455+WillianSozz@users.noreply.github.com>
2023-08-03 15:31:08 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+var fator_lupa = 3;
+
+var entidade = document.getElementById('aft');
+entidade.onclick = function () { this.style.width = (this.clientWidth * fator_lupa) + "px"; };
+entidade.ondblclick = function() {this.style.width = (this.clientWidth / fator_lupa) + "px"; };
+
+var entidade2 = document.getElementById('aft2');
+entidade2.onclick = function () { this.style.width = (this.clientWidth * fator_lupa) + "px"; };
+entidade2.ondblclick = function() {this.style.width = (this.clientWidth / fator_lupa) + "px"; };
+
+var entidade3 = document.getElementById('aft3');
+entidade3.onclick = function () { this.style.width = (this.clientWidth * fator_lupa) + "px"; };
+entidade3.ondblclick = function() {this.style.width = (this.clientWidth / fator_lupa) + "px"; };
+
+var entidade4 = document.getElementById('aft4');
+entidade4.onclick = function () { this.style.width = (this.clientWidth * fator_lupa) + "px"; };
+entidade4.ondblclick = function() {this.style.width = (this.clientWidth / fator_lupa) + "px"; };
+
+var entidade5 = document.getElementById('aft5');
+entidade5.onclick = function () { this.style.width = (this.clientWidth * fator_lupa) + "px"; };
+entidade5.ondblclick = function() {this.style.width = (this.clientWidth / fator_lupa) + "px"; };
+
+var entidade6 = document.getElementById('aft6');
+entidade6.onclick = function () { this.style.width = (this.clientWidth * fator_lupa) + "px"; };
+entidade6.ondblclick = function() {this.style.width = (this.clientWidth / fator_lupa) + "px"; };
